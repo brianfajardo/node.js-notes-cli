4db1afd227b8fe08cdf531aa34d22e62ed8e70c3
brianfajardo <bmf5@hotmail.com>
2017-03-04 15:18:07 -0500

    yargs command functionality

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 // require loads modules from the built-in library
 // https://nodejs.org/api/
 
@@ -11,10 +9,31 @@ const yargs = require('yargs');
 // my modules
 const notes = require('./notes.js');
 
-const argv = yargs.argv
-const command = argv._[0]; /* returns an array containing the command line arguments */
-console.log(`Command: ${command}`);
-console.log('Yargs:', argv)
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note by title', {
+        title: titleOptions
+    })
+    .help()
+    .argv
+const command = argv._[0]; /* returns command line arguments of _ array */
 
 if (command === 'add') {
     let note = notes.addNote(argv.title, argv.body);
@@ -25,7 +44,9 @@ if (command === 'add') {
         console.log(`Note with title "${argv.title}" already exists.`)
     }
 } else if (command === 'list') {
-    notes.getAll()
+    let allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note))
 } else if (command === 'read') {
     let findNote = notes.getNote(argv.title)
     if (findNote) {
